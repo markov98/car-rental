26c5968b8ecb281e436e7b01dee6574de7f50be2
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-28 06:28:28 +0300

    adding template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="main"> <a href="#"><img src="images/logo.gif" alt="" border="0" class="logo" /></a>
+      <ul class="topcar">
+        <li><a href="#"><img src="images/img01.jpg" alt="" width="79" height="92" border="0" /></a></li>
+        <li><a href="#"><img src="images/img02.jpg" alt="" border="0" /></a></li>
+        <li><a href="#"><img src="images/img03.jpg" alt="" border="0" /></a></li>
+        <li><a href="#"><img src="images/img04.jpg" alt="" border="0" /></a></li>
+      </ul>
+      <ul class="topnav">
+        <li><a href="#">Home</a> | </li>
+        <li><a href="#">Customer Care</a> |</li>
+        <li><a href="#">Faq </a> |</li>
+        <li><a href="#">Reservations</a> |</li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <h2 class="gallary">Choose a car from Gallery</h2>
+      <div id="header"> </div>
+      <div id="body">
+        <div class="left"> <img src="images/welcome.gif" alt="" /><br />
+          <p>This is <strong>Car Online</strong> , a free, fully standards-compliant CSS template designed by Free CSS Templates. <br />
+            This free template is released under a Creative Commons Attributions 2.5 license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :) </p>
+          <p><a href="#">Fusce feugiat malesuada odio morbi nunc odio gravida at</a>... tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci.Donec sit amet eros.</p>
+        </div>
+        <form action="#" method="get" class="search">
+          <img src="images/carbooking.gif" alt="" class="head" /><br />
+          <label>Select State :</label>
+          <select class="one">
+            <option>Select State</option>
+          </select>
+          <label>Select Location :</label>
+          <select class="two">
+            <option>Select Locations</option>
+          </select>
+          <label>Pick-Up Date :</label>
+          <select class="three">
+            <option></option>
+          </select>
+          <select class="four">
+            <option></option>
+          </select>
+          <select class="five">
+            <option></option>
+          </select>
+          <a href="#"><img src="images/date.gif" alt="" border="0" class="date" /></a>
+          <label>Drop-Off Date :</label>
+          <select class="three">
+            <option></option>
+          </select>
+          <select class="four">
+            <option></option>
+          </select>
+          <select class="five">
+            <option></option>
+          </select>
+          <a href="#"><img src="images/date.gif" alt="" border="0" class="date" /></a>
+          <input type="submit" class="submit" value="Submit" />
+        </form>
+        <h3 class="tech"><img src="images/our_tech.gif" alt="" width="165" height="16" /></h3>
+        <div class="ads"> <a href="#"><img src="images/ads01.jpg" alt="" width="94" height="45" border="0" /></a><a href="#"><img src="images/ads02.jpg" alt="" border="0" /></a><a href="#"><img src="images/ads03.jpg" alt="" width="80" height="45" border="0" /></a><a href="#"><img src="images/ads04.jpg" alt="" border="0" /></a><a href="#"><img src="images/ads05.jpg" alt="" border="0" /></a><a href="#"><img src="images/ads06.jpg" alt="" width="101" height="45" border="0" /></a><a href="#"><img src="images/ads07.jpg" alt="" width="81" height="45" border="0" /></a><a href="#"><img src="images/ads08.jpg" alt="" width="81" height="45" border="0" /></a></div>
+        <p class="brd"></p>
+        <p class="footer"><a href="#">Home</a> | <a href="#">Customer Care</a> | <a href="#">Faq </a> | <a href="#">Reservations</a> | <a href="#">Contact</a></p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div class="foot"> Copyright Information Goes Here<br />
+        Designed by <a href="http://www.free-css-templates.com/">Free CSS Templates</a>, Thanks to <a href="http://www.dubaiapartments.biz/">Dubai Apartments</a></div>
+      <br class="spacer" />
+    </div>
   )
-}
-
-export default App
+};
