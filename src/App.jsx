27536c885501bd61e50ec6d02108a58bf27a8355
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-29 07:01:35 +0200

    updating css

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@
 export default function App() {
 
   return (
-    <div id="main"> <a href="#"><img src="images/logo.png" alt="" border="0" className="logo" /></a>
-      <ul className="topcar">
-        <li><a href="#"><img src="images/img01.jpg" alt="" width="79" height="92" border="0" /></a></li>
-        <li><a href="#"><img src="images/img02.jpg" alt="" border="0" /></a></li>
-        <li><a href="#"><img src="images/img03.jpg" alt="" border="0" /></a></li>
-        <li><a href="#"><img src="images/img04.jpg" alt="" border="0" /></a></li>
-      </ul>
-      <ul className="topnav">
-        <li><a href="#">Home</a> | </li>
-        <li><a href="#">Customer Care</a> |</li>
-        <li><a href="#">Faq </a> |</li>
-        <li><a href="#">Reservations</a> |</li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <h2 className="gallary">Choose a car from Gallery</h2>
+    <div id="main">
+      <div>
+        <a href="#"><img src="images/logo.png" alt="" border="0" className="logo" /></a>
+        <ul className="topcar">
+          <li><a href="#"><img src="images/img01.jpg" alt="" width="79" height="92" border="0" /></a></li>
+          <li><a href="#"><img src="images/img02.jpg" alt="" border="0" /></a></li>
+          <li><a href="#"><img src="images/img03.jpg" alt="" border="0" /></a></li>
+          <li><a href="#"><img src="images/img04.jpg" alt="" border="0" /></a></li>
+        </ul>
+      </div>
+      <div>
+        <ul className="topnav">
+          <li><a href="#">Home</a> | </li>
+          <li><a href="#">Customer Care</a> |</li>
+          <li><a href="#">Faq </a> |</li>
+          <li><a href="#">Reservations</a> |</li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <h2 className="gallary">Choose a car from Gallery</h2>
+      </div>
       <div id="header"> </div>
       <div id="body">
         <div className="left"> <img src="images/welcome.gif" alt="" /><br />
