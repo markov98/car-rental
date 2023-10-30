ee3e0a9f97611bf36fd75fee7da644227669295f
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-30 08:25:54 +0200

    added body component

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
-import Header from "./Header"
+import Header from "./components/Header"
+import Body from "./components/Body"
 
 export default function App() {
 
   return (
     <div id="main">
       <Header />
-      <div id="body">
-        <div className="left"> <h2>Welcome!</h2> <br />
-          <p>This is <strong>Markov Rentals</strong>, a Car Rental website I have created using React.</p>
-        </div>
-        <form action="#" method="get" className="search">
-          <img src="images/carbooking.gif" alt="" className="head" /><br />
-          <label>Select State :</label>
-          <select className="one">
-            <option>Select State</option>
-          </select>
-          <label>Select Location :</label>
-          <select className="two">
-            <option>Select Locations</option>
-          </select>
-          <label>Pick-Up Date :</label>
-          <select className="three">
-            <option></option>
-          </select>
-          <select className="four">
-            <option></option>
-          </select>
-          <select className="five">
-            <option></option>
-          </select>
-          <a href="#"><img src="images/date.gif" alt="" border="0" className="date" /></a>
-          <label>Drop-Off Date :</label>
-          <select className="three">
-            <option></option>
-          </select>
-          <select className="four">
-            <option></option>
-          </select>
-          <select className="five">
-            <option></option>
-          </select>
-          <a href="#"><img src="images/date.gif" alt="" border="0" className="date" /></a>
-          <input type="submit" className="submit" value="Submit" />
-        </form>
-        <h3 className="tech"><img src="images/our_tech.gif" alt="" width="165" height="16" /></h3>
-        <div className="ads"> <a href="#"><img src="images/ads01.jpg" alt="" width="94" height="45" border="0" /></a><a href="#"><img src="images/ads02.jpg" alt="" border="0" /></a><a href="#"><img src="images/ads03.jpg" alt="" width="80" height="45" border="0" /></a><a href="#"><img src="images/ads04.jpg" alt="" border="0" /></a><a href="#"><img src="images/ads05.jpg" alt="" border="0" /></a><a href="#"><img src="images/ads06.jpg" alt="" width="101" height="45" border="0" /></a><a href="#"><img src="images/ads07.jpg" alt="" width="81" height="45" border="0" /></a><a href="#"><img src="images/ads08.jpg" alt="" width="81" height="45" border="0" /></a></div>
-        <p className="brd"></p>
-        <p className="footer"><a href="#">Home</a> | <a href="#">Customer Care</a> | <a href="#">Faq </a> | <a href="#">Reservations</a> | <a href="#">Contact</a></p>
-      </div>
+      <Body />
       <div className="foot"> Copyright Information Goes Here<br />
         Designed by <a href="http://www.free-css-templates.com/">Free CSS Templates</a>, Thanks to <a href="http://www.dubaiapartments.biz/">Dubai Apartments</a></div>
       <br className="spacer" />
