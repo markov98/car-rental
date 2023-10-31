5fb58943d1707d4b41199bf964278b64cf0fb9cc
markov98 <74935630+markov98@users.noreply.github.com>
2023-10-31 05:03:51 +0200

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Header from "./components/Header"
-import Body from "./components/Body"
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 export default function App() {
 
@@ -7,9 +8,7 @@ export default function App() {
     <div id="main">
       <Header />
       <Body />
-      <div className="foot"> Copyright Information Goes Here<br />
-        Designed by <a href="http://www.free-css-templates.com/">Free CSS Templates</a>, Thanks to <a href="http://www.dubaiapartments.biz/">Dubai Apartments</a></div>
-      <br className="spacer" />
+      <Footer />
     </div>
   )
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="foot">Designed by Martin Markov</div>
+            <br className="spacer" />
+        </>
+    )
+}
