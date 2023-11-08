bc1bce34efe7b62077ee362da3993b98a56b5bd3
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-08 08:28:13 +0200

    added login form component

---
+++ client/src/components/LogInForm.jsx
@@ -0,0 +1,10 @@
+export default function LogInForm() {
+    return (
+        <form action="">
+            <label htmlFor='username'>Username:</label>
+            <input name='username'></input>
+            <label htmlFor='password'>Password:</label>
+            <input name='password'></input>
+        </form>
+    )
+}
