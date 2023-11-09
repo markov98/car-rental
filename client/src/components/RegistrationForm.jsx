59b184283d133b2051650d830cd5408605ee86a9
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-09 20:02:12 +0200

    updated registration form

--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -1,12 +1,65 @@
-export default function RegisterForm() {
+export default function RegistrationForm() {
+    const [formData, setFormData] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+
+      console.log('Form data submitted:', formData);
+    }
+  
     return (
-        <form action="">
-            <h2>Register</h2>
-            <label htmlFor='username'>Username:</label>
-            <input name='username'></input>
-            <label htmlFor='password'>Password:</label>
-            <input name='password'></input>
-            <button type='submit'>Submit</button>
+      <div>
+        <h2>Registration Form</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Register</button>
+          </div>
         </form>
-    )
-}
+      </div>
+    );
+  }
