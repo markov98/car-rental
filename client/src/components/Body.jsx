6a0bd03386b5cbab01e20dc580a017aed268a884
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-10 18:04:37 +0200

    added routes

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from "./Home";
-
+import LoginForm from './LogInForm';
+import RegistrationForm from './RegistrationForm';
 
 export default function Body() {
   return (
     <div id="body">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginForm />} />
+        <Route path='/register' element={<RegistrationForm />} />
+      </Routes>
     </div>
   )
 }
