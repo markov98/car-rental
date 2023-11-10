0ca15ef996b62a21f82290d838892b4415b7a273
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-10 18:14:55 +0200

    replaced a tags with link tags

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     const topCarImg = [
         "images/img01.jpg",
@@ -9,16 +11,16 @@ export default function Header() {
     return (
         <>
             <div className="top">
-                <a href="#"><img src="images/logo.png" alt="" border="0" className="logo" /></a>
+                <Link to="/"><img src="images/logo.png" alt="" border="0" className="logo" /></Link>
                 <ul className="topcar">
-                    {topCarImg.map((el, index) => <li  key={index}><img src={el} /></li>)}
+                    {topCarImg.map((el, index) => <li key={index}><img src={el} /></li>)}
                 </ul>
             </div>
             <div className="nav">
                 <ul className="topnav">
-                    <li><a href="/">Home</a> | </li>
-                    <li><a href="/login">Login</a> |</li>
-                    <li><a href="/register">Register</a> |</li>
+                    <li><Link to="/">Home</Link> | </li>
+                    <li><Link to="/login">Login</Link> |</li>
+                    <li><Link to="/register">Register</Link> |</li>
                 </ul>
                 <h2 className="gallary">Choose a car from Gallery</h2>
             </div>
