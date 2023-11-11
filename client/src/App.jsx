a106260d0f3160a5d3a3ab9433a8be7a415f4467
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-11 16:35:15 +0200

    added app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
+import './style/App.css';
 
 export default function App() {
 
