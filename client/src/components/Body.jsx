a3d3a9c08a3c76af9a9c87282ded0d725bee10e1
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-11 18:40:04 +0200

    added body.css

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import LoginForm from './LogInForm';
 import RegistrationForm from './RegistrationForm';
+import '../style/Body.css';
 
 export default function Body() {
   return (
