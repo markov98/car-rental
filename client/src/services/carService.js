353b71a82716b0c6b616dd5c98ee9e2a4ad5966c
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-21 17:43:02 +0200

    updating carService

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -1,11 +1,6 @@
+import request from "../libreries/request";
 const baseUrl = 'http://localhost:3030/jsonstore/cars';
 
 export const add = async (carData) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify(carData)
-    });
-
-    return await response.json();
+    return await request.post(baseUrl, carData);
 }
