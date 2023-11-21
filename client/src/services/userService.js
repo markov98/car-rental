ed97ca79dc27c3d8ec4a5b13651577d0a512cde5
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-21 19:57:01 +0200

    update userService

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,11 +1,6 @@
+import request from "../libreries/request";
 const baseUrl = 'http://localhost:3030/jsonstore/users';
 
 export const register = async (userData) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify(userData)
-    });
-
-    return await response.json();
+    return await request.post(baseUrl, userData);
 }
