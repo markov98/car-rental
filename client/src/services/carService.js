a78d3321486f2bc28cb7abc115614253a09efade
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-28 00:18:21 +0200

    updated link of car service

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -1,5 +1,5 @@
 import request from "../libraries/request";
-const baseUrl = 'http://localhost:3030/jsonstore/cars';
+const baseUrl = 'http://localhost:3030/data/cars';
 
 export const getAllCars = async () => {
     return await request.get(baseUrl);
