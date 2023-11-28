930a09eb327a6608b77f37b0c231b6268d1be03d
markov98 <74935630+markov98@users.noreply.github.com>
2023-11-29 00:43:50 +0200

    updated nav to change if user is logged in

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 import paths from "../../paths";
 import './Header.css';
 
+
+
 export default function Header() {
+    const { isAuthenticated, username } = useContext(AuthContext)
+
     const topCarImg = [
         "images/img01.jpg",
         "images/img02.jpg",
@@ -21,12 +27,18 @@ export default function Header() {
             <div className="nav">
                 <ul className="topnav">
                     <li><Link to={paths.home}>Home</Link> | </li>
-                    <li><Link to={paths.login}>Login</Link> |</li>
-                    <li><Link to={paths.register}>Register</Link> |</li>
-                    <li><Link to={paths.addCar}>Add Car</Link> |</li>
-                    <li><Link to={paths.carList}>Car List</Link></li>
+                    {isAuthenticated ?
+                        <>
+                            <li><Link to={paths.addCar}>Add Car</Link> |</li>
+                            <li><Link to={paths.carList}>Car List</Link></li>
+                        </> :
+                        <>
+                            <li><Link to={paths.login}>Login</Link> |</li>
+                            <li><Link to={paths.register}>Register</Link> |</li>
+                        </>
+                    }
                 </ul>
-                <h2 className="gallary">Choose a car from Gallery</h2>
+                <h2 className="hello">Hello, {isAuthenticated ? <>{username}</> : <>guest</>}!</h2>
             </div>
             <div id="header"><img src='images/car.png' /></div>
         </>
