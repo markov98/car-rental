3f50a9688bfa3708a3606a0bed6ec06b6b0b9153
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-02 18:51:32 +0200

    replace imageUrl with imgUrl

--- client/src/components/CarList/CarListItem.jsx
+++ client/src/components/CarList/CarListItem.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 
 export default function CarListItem(props) {
-    const { _id, imageUrl, make, price } = props.car;
+    const { _id, imgUrl, make, price } = props.car;
 
     return (
         <div className="car-list-item">
-            <img src={imageUrl} alt={`${make} car`} className="car-image" />
+            <img src={imgUrl} alt={`${make} car`} className="car-image" />
             <div className="car-details">
                 <h3 className="car-make">{make}</h3>
                 <p className="car-price">{`$${price}/Hour`}</p>
