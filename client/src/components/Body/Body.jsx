bcdbdfea80c352e70ddfca3d88e69a83cc5b318b
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-03 20:01:26 +0200

    added error boudary to body

--- client/src/components/Body/Body.jsx
+++ client/src/components/Body/Body.jsx
@@ -8,20 +8,23 @@ import RegistrationForm from '../RegistrationForm/RegistrationForm';
 import AddCar from '../AddCar/AddCar';
 import CarList from '../CarList/CarList';
 import Logout from '../Logout/Logout';
+import ErrorBoundary from '../ErrorBoundary';
 
 import './Body.css';
 
 export default function Body() {
   return (
     <div id="body">
-      <Routes>
-        <Route path={paths.home} element={<Home />} />
-        <Route path={paths.login} element={<LoginForm />} />
-        <Route path={paths.register} element={<RegistrationForm />} />
-        <Route path={paths.logout} element={<Logout />} />
-        <Route path={paths.addCar} element={<AddCar />} />
-        <Route path={paths.carList} element={<CarList />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path={paths.home} element={<Home />} />
+          <Route path={paths.login} element={<LoginForm />} />
+          <Route path={paths.register} element={<RegistrationForm />} />
+          <Route path={paths.logout} element={<Logout />} />
+          <Route path={paths.addCar} element={<AddCar />} />
+          <Route path={paths.carList} element={<CarList />} />
+        </Routes>
+      </ErrorBoundary>
     </div>
   )
 }
--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import useForm from "../../hooks/useForm";
 import { AuthContext } from "../../contexts/AuthContext";
 import './LoginForm.css'
