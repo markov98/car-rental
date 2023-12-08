99f764bd9cbec4505631f1fbebe8d7920cbcb666
markov98 <74935630+markov98@users.noreply.github.com>
2023-12-08 10:14:46 +0200

    updated car list item so that model shows up

--- client/src/components/CarListitem/CarListItem.jsx
+++ client/src/components/CarListitem/CarListItem.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom';
 import './CarListItem.css';
 
 export default function CarListItem(props) {
-    const { _id, imgUrl, make, price } = props.car;
+    const { _id, imgUrl, make, model, price } = props.car;
 
     return (
         <div className="car-list-item">
             <img src={imgUrl} alt={`${make} car`} className="car-image-mini" />
             <div className="car-details-mini">
-                <h3 className="car-make">{make}</h3>
+                <h3 className="car-make">{make} {model}</h3>
                 <p className="car-price">{`$${price}/Hour`}</p>
                 <Link className='details-link' to={`/cars/${_id}/details`}>Details</Link>
             </div>
